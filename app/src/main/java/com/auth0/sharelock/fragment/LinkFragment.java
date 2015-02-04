bd26c426e475de8ba3df10669e423776498c1ff7
Hernan Zalazar <hernanzalazar@gmail.com>
2015-02-04 16:17:01 -0300

    Share functionality

--- app/src/main/java/com/auth0/sharelock/fragment/LinkFragment.java
+++ app/src/main/java/com/auth0/sharelock/fragment/LinkFragment.java
@@ -1,6 +1,8 @@
 package com.auth0.sharelock.fragment;
 
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -8,6 +10,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
@@ -26,10 +29,12 @@ public class LinkFragment extends Fragment {
 
     Secret secret;
     EventBus bus;
+    Uri link;
 
     TextView linkText;
     ProgressBar progressBar;
     Button retryButton;
+    ImageButton shareButton;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -65,7 +70,7 @@ public class LinkFragment extends Fragment {
         TextView secretText = (TextView) view.findViewById(R.id.link_secret_text);
         secretText.setText(secret.getSecret());
         ShareEditText shareEditText = (ShareEditText) view.findViewById(R.id.link_share_list);
-        shareEditText.setEnabled(false);
+        shareEditText.setFocusable(false);
         for (String viewer: secret.getAllowedViewers()) {
             shareEditText.addObject(viewer);
         }
@@ -76,8 +81,17 @@ public class LinkFragment extends Fragment {
             @Override
             public void onClick(View v) {
                 bus.post(new RequestLinkEvent(secret));
-                retryButton.setVisibility(View.GONE);
-                progressBar.setVisibility(View.VISIBLE);
+            }
+        });
+        shareButton = (ImageButton) view.findViewById(R.id.link_share_button);
+        shareButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent sendIntent = new Intent();
+                sendIntent.setAction(Intent.ACTION_SEND);
+                sendIntent.putExtra(Intent.EXTRA_TEXT, link.toString());
+                sendIntent.setType("text/plain");
+                startActivity(Intent.createChooser(sendIntent, getString(R.string.share_link_chooser_title)));
             }
         });
     }
@@ -85,12 +99,21 @@ public class LinkFragment extends Fragment {
     public void onEvent(NewLinkEvent event) {
         bus.removeStickyEvent(event);
         progressBar.setVisibility(View.GONE);
-        linkText.setText(event.getLink().toString());
+        shareButton.setVisibility(View.VISIBLE);
+        link = event.getLink();
+        linkText.setText(link.toString());
     }
 
     public void onEventMainThread(SharelockAPIErrorEvent event) {
         linkText.setText(R.string.link_generation_failed_message);
         progressBar.setVisibility(View.GONE);
         retryButton.setVisibility(View.VISIBLE);
+        shareButton.setVisibility(View.GONE);
+    }
+
+    public void onEvent(RequestLinkEvent event) {
+        retryButton.setVisibility(View.GONE);
+        progressBar.setVisibility(View.VISIBLE);
+        shareButton.setVisibility(View.GONE);
     }
 }
